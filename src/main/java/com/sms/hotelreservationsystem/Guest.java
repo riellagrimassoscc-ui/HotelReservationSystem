4793eabb5cadf003f8d3f476c012e16758aa67fa
Riel So <skt.bang.315@gmail.com>
2026-09-10 20:59:24 +0800

    Update to Hotel Reservation System

---
+++ src/main/java/com/sms/hotelreservationsystem/Guest.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sms.hotelreservationsystem;
+
+/**
+ *
+ * @author Riel
+ */
+public class Guest {
+        private int guestID;
+        private String name;
+        private String phoneNum;
+        private String email;
+
+    
+}
